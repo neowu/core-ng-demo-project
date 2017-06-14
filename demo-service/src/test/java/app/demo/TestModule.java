eb40dd7d55b89e5f6f5bf39aec533fe0fb146dfb
neo <neowu.us@gmail.com>
2017-06-14 14:54:01 -0400

    refactor route config

--- demo-service/src/test/java/app/demo/TestModule.java
+++ demo-service/src/test/java/app/demo/TestModule.java
@@ -2,8 +2,6 @@ package app.demo;
 
 import app.DemoServiceApp;
 import core.framework.api.AbstractTestModule;
-import core.framework.api.http.HTTPClient;
-import core.framework.api.http.HTTPClientBuilder;
 
 /**
  * @author neo
@@ -11,7 +9,7 @@ import core.framework.api.http.HTTPClientBuilder;
 public class TestModule extends AbstractTestModule {
     @Override
     protected void initialize() {
-        overrideBinding(HTTPClient.class, new HTTPClientBuilder().build());
+//        overrideBinding(HTTPClient.class, new HTTPClientBuilder().build());
 
         load(new DemoServiceApp());
     }
