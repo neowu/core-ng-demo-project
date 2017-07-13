09cee208a60f287e82ff56fd756f88c7e63104b9
neo <neowu.us@gmail.com>
2017-07-13 12:38:05 +0800

    * kafka: update to 0.11.0.0, use built-in headers for meta data

--- demo-service/src/main/java/app/ProductModule.java
+++ demo-service/src/main/java/app/ProductModule.java
@@ -3,7 +3,7 @@ package app;
 import app.demo.api.ProductWebService;
 import app.demo.api.product.ProductView;
 import app.demo.api.product.kafka.ProductUpdatedMessage;
-import app.demo.product.kafka.ProductUpdatedMessageHandler;
+import app.demo.product.kafka.ProductUpdatedBulkMessageHandler;
 import app.demo.product.service.ProductService;
 import app.demo.product.web.ProductUpdatedMessageTestController;
 import app.demo.product.web.ProductWebServiceImpl;
@@ -33,7 +33,7 @@ public class ProductModule extends Module {
     private void configureKafka() {
         kafka().uri("localhost:9092");
 
-        kafka().subscribe("product-updated", ProductUpdatedMessage.class, bind(ProductUpdatedMessageHandler.class))
+        kafka().subscribe("product-updated", ProductUpdatedMessage.class, bind(ProductUpdatedBulkMessageHandler.class))
                .poolSize(2);
 
         kafka().publish("product-updated", ProductUpdatedMessage.class);
