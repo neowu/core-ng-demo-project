f30d745ee28ace79b9001f90ada7bc56bb7100e1
neo <neowu.us@gmail.com>
2018-04-10 12:30:33 -0400

    * http: support allow /_sys/* url by cidr, via sys.http.management.allowCIDR property

--- demo-site/src/main/java/app/DemoSiteApp.java
+++ demo-site/src/main/java/app/DemoSiteApp.java
@@ -2,7 +2,6 @@ package app;
 
 import core.framework.module.App;
 import core.framework.module.SystemModule;
-import core.framework.util.Sets;
 
 /**
  * @author neo
@@ -14,7 +13,7 @@ public class DemoSiteApp extends App {
 
         http().enableGZip();
         http().httpsPort(8443);
-        http().allowClientIP(Sets.newHashSet("123.123.123.123/32"));
+
         site().enableWebSecurity();
         log().maskFields("password");
 
