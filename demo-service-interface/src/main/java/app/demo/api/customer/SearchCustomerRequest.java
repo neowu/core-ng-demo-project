6a80af00b54e6179393571517a1586f254f946f6
neo <neowu.us@gmail.com>
2018-07-27 09:41:42 +0800

    update demo to latest core

--- demo-service-interface/src/main/java/app/demo/api/customer/SearchCustomerRequest.java
+++ demo-service-interface/src/main/java/app/demo/api/customer/SearchCustomerRequest.java
@@ -1,11 +1,14 @@
 package app.demo.api.customer;
 
+import core.framework.api.validate.NotNull;
 import core.framework.api.web.service.QueryParam;
 
 public class SearchCustomerRequest {
+    @NotNull
     @QueryParam(name = "skip")
     public Integer skip = 0;
 
+    @NotNull
     @QueryParam(name = "limit")
     public Integer limit = 1000;
 
