a258b81ecb3e609a06480df228fc827a084feff1
neo <neowu.us@gmail.com>
2018-12-03 10:18:06 +0800

    * elasticsearch: added ElasticSearchMigration to support migration

--- demo-es-migration/src/main/java/Main.java
+++ demo-es-migration/src/main/java/Main.java
@@ -1,13 +1,11 @@
 import core.framework.search.ElasticSearchMigration;
 import core.framework.util.ClasspathResources;
 
-import java.io.IOException;
-
 /**
  * @author neo
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         var migration = new ElasticSearchMigration("sys.properties");
         migration.migrate(search -> search.createIndex("product", ClasspathResources.text("product-index.json")));
     }
