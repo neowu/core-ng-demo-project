db41570d12d5fd35213dc3d2b82e2a34790eba6a
neo <neowu.us@gmail.com>
2018-12-25 11:52:24 -0500

    experiment jenkins scm polling with included region

--- demo-service/src/main/java/app/DemoServiceApp.java
+++ demo-service/src/main/java/app/DemoServiceApp.java
@@ -9,9 +9,10 @@ import core.framework.module.SystemModule;
 public class DemoServiceApp extends App {
     @Override
     protected void initialize() {
+        http().httpsPort(8443);
         load(new SystemModule("sys.properties"));
         load(new ProductModule());
         load(new JobModule());
-        load(new CustomerModule());
+//        load(new CustomerModule());
     }
 }
