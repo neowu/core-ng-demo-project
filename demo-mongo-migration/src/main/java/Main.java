85986bb7cc37025ebbe7e46cee8539d3fbd1e588
neo <neowu.us@gmail.com>
2019-07-22 12:56:02 -0400

    * mongo: support mongo migration

--- demo-mongo-migration/src/main/java/Main.java
+++ demo-mongo-migration/src/main/java/Main.java
@@ -1,12 +1,18 @@
 import com.mongodb.client.model.Indexes;
 import core.framework.mongo.MongoMigration;
+import org.bson.Document;
 
 /**
  * @author neo
  */
 public class Main {
     public static void main(String[] args) {
-        var migration = new MongoMigration("sys.properties");
+        var migration = new MongoMigration("sys.properties", "sys.mongo.adminURI");
+        migration.migrate(mongo -> {
+            mongo.runCommand(new Document().append("setParameter", 1).append("notablescan", 1));
+        });
+
+        migration = new MongoMigration("sys.properties");
         migration.migrate(mongo -> {
             mongo.createIndex("test", Indexes.ascending("name"));
         });
