be933e99bde020b7645906b9d83b35242df5aae4
neo <neowu.us@gmail.com>
2019-11-01 11:44:14 -0400

    * search: update ElasticSearch.createIndex to ElasticSearch.putIndex, to support create index or update mappings, for es migration

--- demo-es-migration/src/main/java/Main.java
+++ demo-es-migration/src/main/java/Main.java
@@ -7,6 +7,6 @@ import core.framework.util.ClasspathResources;
 public class Main {
     public static void main(String[] args) {
         var migration = new ElasticSearchMigration("sys.properties");
-        migration.migrate(search -> search.createIndex("product", ClasspathResources.text("product-index.json")));
+        migration.migrate(search -> search.putIndex("product", ClasspathResources.text("product-index.json")));
     }
 }
