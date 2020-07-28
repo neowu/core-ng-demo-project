baa3e352f1c2d05349f09514cd07d6585ed3b57f
neo <neowu.us@gmail.com>
2020-07-28 00:19:23 -0400

    update example db migration

--- demo-service-db-migration/src/main/resources/db/migration/V1.1__init-tables.sql
+++ demo-service-db-migration/src/main/resources/db/migration/V1.1__init-tables.sql
@@ -1,9 +1,10 @@
-CREATE TABLE IF NOT EXISTS `customer` (
-  `id`           INT AUTO_INCREMENT,
-  `status`       ENUM ('ACTIVE', 'INACTIVE') NOT NULL,
-  `email`        VARCHAR(50)                 NOT NULL UNIQUE,
-  `first_name`   VARCHAR(50)                 NOT NULL,
-  `last_name`    VARCHAR(50),
-  `updated_time` DATETIME                    NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS customer
+(
+    id           INT AUTO_INCREMENT,
+    status       ENUM ('ACTIVE', 'INACTIVE') NOT NULL,
+    email        VARCHAR(50)                 NOT NULL UNIQUE,
+    first_name   VARCHAR(50)                 NOT NULL,
+    last_name    VARCHAR(50),
+    updated_time DATETIME                    NOT NULL,
+    PRIMARY KEY (id)
 );
