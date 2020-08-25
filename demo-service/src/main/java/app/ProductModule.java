3e0f85287278d2d2ca0e7914151fcadc4a5af521
neo <neowu.us@gmail.com>
2020-08-25 14:15:39 -0400

    * stats: collect thread dump via stats.info if cpu usage is high

--- demo-service/src/main/java/app/ProductModule.java
+++ demo-service/src/main/java/app/ProductModule.java
@@ -27,7 +27,7 @@ public class ProductModule extends Module {
 
         bind(ProductService.class);
 
-        configureKafka();
+//        configureKafka();
 
         api().service(ProductWebService.class, bind(ProductWebServiceImpl.class));
 
