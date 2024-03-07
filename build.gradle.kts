788364c77374cd60cba7b4640c4b15099d7e1b6b
neo <1100909+neowu@users.noreply.github.com>
2024-03-07 13:56:11 +0800

    release 9.0.8
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,6 @@ project(":demo-service") {
         implementation(project(":demo-service-interface"))
         implementation("core.framework:core-ng-mongo:${coreNGVersion}")
         implementation("core.framework:core-ng-search:${coreNGVersion}")
-        implementation("com.squareup.okhttp3:okhttp-sse:4.11.0")
         testImplementation("core.framework:core-ng-mongo-test:${coreNGVersion}")
         testImplementation("core.framework:core-ng-search-test:${coreNGVersion}")
         runtimeOnly("core.framework.mysql:mysql-connector-j:8.3.0-r1")
