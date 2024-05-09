89d90f8b74bbd12d21c8eb9e9cc2e7fa14e62394
neo <1100909+neowu@users.noreply.github.com>
2024-05-09 12:33:20 +0800

    update mysql driver
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,7 +91,7 @@ project(":demo-service") {
         implementation("com.squareup.okhttp3:okhttp-sse:4.12.0@jar")
         testImplementation("core.framework:core-ng-mongo-test:${coreNGVersion}")
         testImplementation("core.framework:core-ng-search-test:${coreNGVersion}")
-        runtimeOnly("core.framework.mysql:mysql-connector-j:8.4.0-r1")
+        runtimeOnly("core.framework.mysql:mysql-connector-j:8.4.0-r2")
 //        runtimeOnly("org.postgresql:postgresql:42.7.1")
         testRuntimeOnly("org.hsqldb:hsqldb:${hsqlVersion}")
     }
