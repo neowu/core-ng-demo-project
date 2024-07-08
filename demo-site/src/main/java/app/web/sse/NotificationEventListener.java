c0d451f8795397ef61e1c539b639d434671c7f9c
neo <1100909+neowu@users.noreply.github.com>
2024-07-08 11:50:35 +0800

    review and refactor sse
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- demo-site/src/main/java/app/web/sse/NotificationEventListener.java
+++ demo-site/src/main/java/app/web/sse/NotificationEventListener.java
@@ -1,14 +1,28 @@
 package app.web.sse;
 
+import core.framework.log.ActionLogContext;
+import core.framework.util.Threads;
 import core.framework.web.Request;
 import core.framework.web.sse.Channel;
 import core.framework.web.sse.ChannelListener;
 
+import java.time.Duration;
+
 public class NotificationEventListener implements ChannelListener<NotificationEvent> {
     @Override
     public void onConnect(Request request, Channel<NotificationEvent> channel, String lastEventId) {
-        System.err.println(lastEventId);
+        ActionLogContext.triggerTrace(true);
+
         channel.join("group1");
+
+        for (int i = 0; i < 10; i++) {
+            final NotificationEvent event = new NotificationEvent();
+            event.text = "on connect " + i;
+            channel.send(event);
+            Threads.sleepRoughly(Duration.ofSeconds(1));
+        }
+
+//        channel.close();
 //        throw new Error("test error");
     }
 }
