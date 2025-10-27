26fc2d82c5466a151e36eb296e00f3988664e9ad
neo <1100909+neowu@users.noreply.github.com>
2025-10-27 14:15:31 +0800

    revert custom LogLevels impl, not worth to add complexity, keep original simpler design
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- demo-site/src/main/java/Main.java
+++ demo-site/src/main/java/Main.java
@@ -1,13 +1,8 @@
 import app.DemoSiteApp;
-import core.framework.log.LogLevel;
-import core.framework.log.LogLevels;
 
 /**
  * @author neo
  */
 void main() {
-    LogLevels.add("com.test.", LogLevel.WARN, LogLevel.ERROR);
-    LogLevels.add("com.test2.", LogLevel.WARN, LogLevel.WARN);
-
     new DemoSiteApp().start();
 }
